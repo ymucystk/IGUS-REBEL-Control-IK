2812e4a0856f9aa5a4922e8910e4706bd8f0e2a8
Yamauchi Yoshitaka <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2025-05-14 13:39:22 +0900

    normalize180 correction

--- src/app/home.js
+++ src/app/home.js
@@ -469,6 +469,9 @@ export default function Home(props) {
       return angle
     }
     const amari = angle % 180
+    if(amari === 0){
+      return amari
+    }else
     if(amari < 0){
       return (180 + amari)
     }else{
